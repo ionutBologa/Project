62ce7cef362c80447b029e1fb99e6532f123f29a
ionutBologa <bologa.ionut13@gmail.com>
2023-07-29 11:36:42 +0300

    final working Hours

--- connections.py
+++ connections.py
@@ -1,4 +1,8 @@
+import os
+import smtplib,ssl
+from flask import Flask, request
 import mysql.connector
+from email.message import EmailMessage
 
 connect=mysql.connector.connect(
         host="localhost",
@@ -8,3 +12,24 @@ connect=mysql.connector.connect(
         auth_plugin='mysql_native_password'
     )
 cursor=connect.cursor()
+app= Flask(__name__)
+
+def send_email(to, subject, message):
+
+    email_address = "bologa.ionut13@gmail.com"
+    email_password = "ozotlvccastoqrhb"
+
+    # create email
+    msg = EmailMessage()
+    msg['Subject'] = subject
+    msg['From'] = email_address
+    msg['To'] = to
+    msg.set_content(message)
+
+    # send email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_address, email_password)
+        smtp.send_message(msg)
+
+
+
--- worked_hours.py
+++ worked_hours.py
@@ -1,65 +1,76 @@
-from datetime import datetime
-from connections import connect
-
-
-
-
+from flask import request
+import time
+from connections import connect,app,send_email
 from datetime import datetime
 
-from datetime import datetime
 
 def worked_hours():
     mycursor = connect.cursor()
     mycursor.execute("SELECT * FROM check_times")
     mydata = mycursor.fetchall()
 
-    id_entries = []
+    today=datetime.today().date()
+    check_in = []
+    check_out= []
     entry_worked_hours = {}
-
+    worked=""
 
     for entry in mydata:
         entry_id = entry[0]
         entry_date_time = entry[1]
-        entry_way = entry[2]
+        entry_way = entry[2].strip(";")
 
         date_time_obj = datetime.strptime(str(entry_date_time), "%Y-%m-%d %H:%M:%S")
-
-
         entries = {
             "id": entry_id,
             "date_time": date_time_obj,
             "way": entry_way
         }
-        id_entries.append(entries)
 
-    # Process the data to calculate and print the worked hours for each entry_id
+        if entries["way"] == "in":
+            check_in.append(entries)
+        if entries["way"] == "out":
+            check_out.append(entries)
 
-    for entry in id_entries:
+    # Process the data to calculate the worked hours for each entry_id
+
+    for entry in check_in:
         entry_id = entry["id"]
         date_time_obj = entry["date_time"]
-        entry_way = entry["way"]
-
 
         if entry_id not in entry_worked_hours:
-            entry_worked_hours[entry_id] = {"in_time": date_time_obj, "out_time": date_time_obj}
+            entry_worked_hours[entry_id] = {"in_time": date_time_obj, "out_time": None}
+        else:
+            entry_worked_hours[entry_id]["in_time"] = date_time_obj
 
-            if entry_way == 'in':
-                entry_worked_hours[entry_id]["in_time"] = date_time_obj
-            elif entry_way == 'out':
-                 entry_worked_hours[entry_id]["out_time"] = date_time_obj
+    for entry in check_out:
+        entry_id = entry["id"]
+        date_time_obj = entry["date_time"]
 
-    print(entry_worked_hours)
-    # Calculate and print the worked hours for each entry_id
+        if entry_id not in entry_worked_hours:
+            entry_worked_hours[entry_id] = {"in_time": None, "out_time": date_time_obj}
+        else:
+            entry_worked_hours[entry_id]["out_time"] = date_time_obj
+
+    # Calculate  the worked hours for each entry_id
     for entry_id, timestamps in entry_worked_hours.items():
         time_in = timestamps["in_time"]
         time_out = timestamps["out_time"]
 
         if time_in and time_out:
-            worked_hours = time_out - time_in
-            print(f"ID: {entry_id}, Worked Hours: {worked_hours.total_seconds() / 3600:2} hours")
-        else:
-            print(f"ID {entry_id} has missing 'in' or 'out' event.")
+            worked_hours = (time_out - time_in).total_seconds() / 3600
+            if worked_hours < 28799/3600:
+                worked+=f"ID: {entry_id} on date :{today} worked {worked_hours} hours\n"
+                mycursor.execute(f"Insert into lessThen8Hours value('{entry_id}','{today}','{worked_hours}')")
+                connect.commit()
+    email="bologaionutviorel@gmail.com"
+    email1="bologa.raluca@gmail.com"
+    content=" Employees that worked  less then 8 hours today"
+    subject = ""+worked
+
+
+    send_email((email,email1),content,subject)
+
 
 
 worked_hours()
-
